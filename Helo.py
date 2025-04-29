ce2cd8252f3733a4eb58713d96a70a029fbb8986
Shreyas Shridhar Kulkarni <108882647+Shreyu-07@users.noreply.github.com>
2025-04-29 21:28:58 +0530

    Create Helo.py

---
+++ Helo.py
@@ -0,0 +1,23 @@
+#printing hello world using function():
+
+def hello():
+   return ("Hello World!")
+a=hello()
+print(a)
+
+#printing hello world using normal python:
+
+print("Hello World!")
+
+#printing hello world using switch statement:
+
+def switchcase(case):
+    match case:
+        case 1:
+            return "hello world!"
+print(switchcase(1))
+
+#printing hello world by assigning variables
+
+a = "hello world!"
+print(a)
